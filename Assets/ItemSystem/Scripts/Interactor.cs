61a066aa091c27de7b9f57e6232640e0aca7aa46
karldee93 <71699122+karldee93@users.noreply.github.com>
2021-10-07 14:50:56 +0100

    Update Interactor.cs

--- Assets/ItemSystem/Scripts/Interactor.cs
+++ Assets/ItemSystem/Scripts/Interactor.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class Interactor : MonoBehaviour
 {
+    public Inventory attachedInventory;
+
+    Transform actorCamera;
+    LayerMask layerMask;
+
+    [SerializedField]
+    private float maxDistanceFromCameraToObjects = 100f;
+
+
+    [SerializeField]
+    private float interactableDistance = 3f;
+    private float distanceToInteractable;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        layerMask = gameObject.layer;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Interact();
+    }
+
+    public void Interact()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            actorCamera = Camera.main.transform;
+
+        }
     }
 }
